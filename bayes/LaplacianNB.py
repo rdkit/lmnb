19ec5006bce0e31857d29d7cec99cb65ce125194
Greg Landrum <greg.landrum@gmail.com>
2023-01-06 09:08:13 +0100

    Get the code working with sklearn-1.2.0
    The NaiveBayes classifiers now have a force_alpha attribute, which is explained in:
    https://github.com/scikit-learn/scikit-learn/pull/22269
    
    This PR just gets the code working; it would be good to invest some time into reading the details of that sklearn PR and making the appropriate adjustments.

--- bayes/LaplacianNB.py
+++ bayes/LaplacianNB.py
@@ -85,7 +85,9 @@ class LaplacianNB(_BaseDiscreteNB):
     >>> print(clf.predict(X[2:3]))
     [3]
     """
-
+    # see https://github.com/scikit-learn/scikit-learn/pull/22269 for an explanation
+    force_alpha = False
+    
     def __init__(self, *, alpha=1.0, fit_prior=True, class_prior=None):
         self.alpha = alpha
         self.fit_prior = fit_prior
